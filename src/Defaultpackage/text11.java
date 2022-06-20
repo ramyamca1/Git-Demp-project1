def7723716dbef378f7632bb092a2b519c3582b8
ramyamca1 <102754939+ramyamca1@users.noreply.github.com>
2022-06-20 15:25:19 +0530

    First commit....

---
+++ src/Defaultpackage/text11.java
@@ -0,0 +1,17 @@
+package Defaultpackage;
+
+public class text11 {
+	
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		if(hungry=="yes")
+			System.out.println("eat something");
+		else
+			System.out.println("Do work");
+	}
+}
+	
+		 
+	
+
